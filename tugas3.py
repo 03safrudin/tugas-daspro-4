2237e30b9a253e78b4eb064570b6331f88e26688
03safrudin <164322470+03safrudin@users.noreply.github.com>
2024-03-26 20:12:02 +0700

    tugas3.py

--- tugas3.py
+++ tugas3.py
@@ -1,7 +1,14 @@
-belanja = int(input("harga belanja : "))
-if belanja >=250000:
-    diskon = belanja*0.5
-    print ("Anda mendapatkan diskon\nHarga belanja menjadi : ",belanja-diskon)
+print("apakah anda mahaiswa unkhair ? (y/t)")
+test = input(">> ")
+if test == "y":
+    username = input("Username : ")
+    password = input("Password : ")
+
+    if username == "Safrudin" and password == "udin123":
+        print("Nama : Safrudin\nNpm : 07352211116\nMata Kuliah : Dasar Pemrograman")
+    elif username == "Bakhtiar" and password == "tiar123":
+        print("Nama : Bakhtiar\nNpm : 07352211122\nMata Kuliah : Dasar Pemrograman")
+    else :
+        print("gagal login")
 else :
-    print("Belanjaan anda kurang dari Rp 250.000, tidak ada diskon")
-    print("Harga belanjaan anda : ",belanja)
+    print("terima kasih")
